d1712b9eb76e8a50399246189df70aaab640feac
Dustin Pho <dustinpho@gmail.com>
2025-09-25 14:57:57 -0400

    core: get rid of inflight_fut thing, more tests

--- cfs-core/src/file.rs
+++ cfs-core/src/file.rs
@@ -1,12 +1,17 @@
 use crate::{
     ByteRange,
-    read::{BytesFuture, ChunkedVolumeStore, ReadAheadPolicy},
+    read::{BytesFuture, ChunkedVolumeStore, ReadAheadPolicy, ReadError},
 };
 use futures::FutureExt;
 use std::{cmp::min, io::SeekFrom, pin::Pin, sync::Arc, task::Poll};
 use tokio::io::{AsyncRead, AsyncSeek, ReadBuf};
 
-/// Handle for a file on a volume.
+enum State {
+    Loading(BytesFuture),
+    Ready(bytes::Bytes),
+}
+
+/// Handle for a file on a volume. Not thread-safe.
 // TODO: this only handles reads for a file that lives completely on one volume. if it spans two
 // volumes, then we need to keep a list of potential volumes and per-volume ranges that we'll
 // iterate over.
@@ -26,27 +31,19 @@ pub struct VolumeFile {
     /// Byte range for reads in `chunk`, offsets are relative to `chunk`.
     chunk_read_range: ByteRange,
 
-    /// Future that returns the current chunk (bytes) when we've finished reading from object
-    /// storage. If the chunk is already cached, this future resolves immediately.
-    chunk: BytesFuture,
+    /// State of the current chunk. Either a future that will resolve into Bytes (which represents
+    /// the chunk) or the Bytes backing the chunk itself.
+    state: State,
+
+    /// Number of errors encountered while trying to read this chunk. Reset after each successful
+    /// read.
+    num_read_errors: u8,
 
     /// If provided, performs readaheads based on the given policy
     readahead: Option<ReadAheadPolicy>,
 
     /// Handle into our chunked object store cache.
     volume_chunk_store: Arc<ChunkedVolumeStore>,
-
-    /// Number of errors encountered while trying to read this chunk.
-    num_read_errors: u8,
-
-    /// We need to conditionally keep a second copy of Shared<Future<_>> here to allow to polling
-    /// the future multiple times. Shared<Future<_>> doesn't allow you to poll an already completed
-    /// future, but we do need this functionality if the read-buffer is smaller than our chunk
-    /// size. In those scenarios, the kernel might map multiple reads into a single object storage
-    /// range read. This inflight_fut is kept around because we need to keep a reference to the
-    /// waker (attached to the std::task::Context) alive so it wake up our AsyncRead/AsyncSeek.
-    /// Yeah, this is ugly ... not sure what else we can do though.
-    inflight_fut: Option<BytesFuture>,
 }
 
 impl VolumeFile {
@@ -77,11 +74,10 @@ impl VolumeFile {
             volume_read_cursor: volume_read_range.offset,
             volume_read_range,
             chunk_read_range: relative_chunk_range,
-            chunk,
+            state: State::Loading(chunk),
             readahead,
             volume_chunk_store,
             num_read_errors: 0,
-            inflight_fut: None,
         }
     }
 
@@ -111,7 +107,62 @@ impl VolumeFile {
                 self.volume_read_range.end() - self.volume_read_cursor,
             ),
         };
-        self.chunk = chunk;
+
+        self.state = State::Loading(chunk);
+    }
+
+    fn poll_chunk(
+        &mut self,
+        cx: &mut std::task::Context<'_>,
+    ) -> Poll<std::io::Result<bytes::Bytes>> {
+        loop {
+            match &self.state {
+                State::Ready(bytes) => return Poll::Ready(Ok(bytes.clone())),
+                State::Loading(fut) => {
+                    let Poll::Ready(result) = fut.clone().poll_unpin(cx) else {
+                        return Poll::Pending;
+                    };
+
+                    match self.try_load_chunk(result)? {
+                        Some(bytes) => return Poll::Ready(Ok(bytes)),
+                        None => continue, // retry after re-seeking
+                    }
+                }
+            }
+        }
+    }
+
+    fn try_load_chunk(
+        &mut self,
+        poll_result: Result<bytes::Bytes, ReadError>,
+    ) -> std::io::Result<Option<bytes::Bytes>> {
+        match poll_result {
+            Ok(bytes) => {
+                self.num_read_errors = 0;
+                self.state = State::Ready(bytes.clone());
+                Ok(Some(bytes))
+            }
+            // enough bad reads that we're just giving up
+            Err(e) if self.num_read_errors >= Self::READ_ERROR_THRESHOLD => {
+                Err(std::io::Error::new(
+                    std::io::ErrorKind::Interrupted,
+                    format!(
+                        "failed to seek to file pos {} (pos {} in volume): {e}",
+                        self.relative_read_cursor(),
+                        self.volume_read_cursor
+                    ),
+                ))
+            }
+            // bad read, but try again (by re-seeking which resets the chunk)
+            Err(_) => {
+                self.num_read_errors += 1;
+                let mut pinned = std::pin::pin!(self);
+                match pinned.as_mut().start_seek(SeekFrom::Current(0)) {
+                    Ok(()) => Ok(None),
+                    Err(e) => Err(e),
+                }
+            }
+        }
     }
 
     /// Returns the offset of what we've seeked to, relative to the start of the file.
@@ -134,76 +185,41 @@ impl AsyncRead for VolumeFile {
         let mut_inner = self.get_mut();
 
         loop {
-            // read_range is exhausted, return Ready(Ok(())) to indicate EOF.
-            if mut_inner.finished() {
+            // read_range is exhausted or there's no space in the buffer, we're done
+            if mut_inner.finished() || buf.remaining() == 0 {
                 return Poll::Ready(Ok(()));
             }
+
+            let bytes = match mut_inner.poll_chunk(cx) {
+                Poll::Ready(Ok(bytes)) => bytes,
+                Poll::Ready(Err(e)) => return Poll::Ready(Err(e)),
+                Poll::Pending => return Poll::Pending,
+            };
 
             let buf_capacity = buf.remaining();
+            if buf_capacity as u64 >= mut_inner.chunk_read_range.len {
+                // can read the entire remaining chunk into the buf. load the next chunk
+                // after pushing these into the buf, to see if we can jam more things into
+                // it.
+                buf.put_slice(&bytes.slice(mut_inner.chunk_read_range.as_range_usize()));
+                mut_inner.volume_read_cursor += mut_inner.chunk_read_range.len;
+                mut_inner.load_next_chunk();
 
-            // no more space in buffer, we're done
-            if buf_capacity == 0 {
+                // since we haven't finished pushing everything we can into the buf, continue on in
+                // a loop. if we return Poll::Ready(Ok(())) with a buffer that isn't full then
+                // we're signaling that it's an EOF. so instead we loop over again here
+                continue;
+            } else {
+                // partial read of the chunk since buf is too small for the chunk. push what we
+                // can and update the internal cursor + range.
+                buf.put_slice(&bytes.slice(std::ops::Range {
+                    start: mut_inner.chunk_read_range.offset as usize,
+                    end: mut_inner.chunk_read_range.offset as usize + buf_capacity,
+                }));
+                mut_inner.volume_read_cursor += buf_capacity as u64;
+                mut_inner.chunk_read_range.advance(buf_capacity as u64);
+
                 return Poll::Ready(Ok(()));
-            }
-
-            // we keep a separate copy around each time we need to poll because (1) creating a new
-            // copy of the Shared<Future<_>> each time doesn't keep the waker around and (2)
-            // Shared<Future<_>> doesn't allow you to poll it if it's already completed .
-            // copying a shared future is cheap though! but this is ugly.
-            let chunk_handle = mut_inner
-                .inflight_fut
-                .get_or_insert_with(|| mut_inner.chunk.clone());
-
-            let Poll::Ready(result) = chunk_handle.poll_unpin(cx) else {
-                return Poll::Pending;
-            };
-            mut_inner.inflight_fut.take();
-
-            match result {
-                Ok(bytes) => {
-                    // successful read, reset num_read_errors
-                    mut_inner.num_read_errors = 0;
-
-                    if buf_capacity as u64 >= mut_inner.chunk_read_range.len {
-                        // can read the entire remaining chunk into the buf. load the next chunk
-                        // after pushing these into the buf, to see if we can jam more things into
-                        // it.
-                        buf.put_slice(&bytes.slice(mut_inner.chunk_read_range.as_range_usize()));
-                        mut_inner.volume_read_cursor += mut_inner.chunk_read_range.len;
-                        mut_inner.load_next_chunk();
-
-                        continue;
-                    } else {
-                        // partial read of the chunk since buf is too small for the chunk. push what we
-                        // can and update the internal cursor + range.
-                        buf.put_slice(&bytes.slice(std::ops::Range {
-                            start: mut_inner.chunk_read_range.offset as usize,
-                            end: mut_inner.chunk_read_range.offset as usize + buf_capacity,
-                        }));
-                        mut_inner.volume_read_cursor += buf_capacity as u64;
-                        mut_inner.chunk_read_range.advance(buf_capacity as u64);
-
-                        return Poll::Ready(Ok(()));
-                    }
-                }
-                Err(e) if mut_inner.num_read_errors >= Self::READ_ERROR_THRESHOLD => {
-                    return Poll::Ready(Err(std::io::Error::new(
-                        std::io::ErrorKind::Interrupted,
-                        format!(
-                            "failed to seek to file pos {} (pos {} in volume): {e}",
-                            mut_inner.relative_read_cursor(),
-                            mut_inner.volume_read_cursor
-                        ),
-                    )));
-                }
-                Err(_) => {
-                    mut_inner.num_read_errors += 1;
-                    let mut pinned = std::pin::pin!(mut_inner);
-                    match pinned.as_mut().start_seek(SeekFrom::Current(0)) {
-                        Ok(()) => return Poll::Pending,
-                        Err(e) => return Poll::Ready(Err(e)),
-                    }
-                }
             }
         }
     }
@@ -249,7 +265,7 @@ impl AsyncSeek for VolumeFile {
                 mut_inner.volume_read_range.end() - new_read_cursor,
             ),
         };
-        mut_inner.chunk = chunk;
+        mut_inner.state = State::Loading(chunk);
 
         Ok(())
     }
@@ -265,39 +281,10 @@ impl AsyncSeek for VolumeFile {
 
         let mut_inner = self.get_mut();
 
-        // we keep a separate copy around each time we need to poll because (1) creating a new copy
-        // of the Shared<Future<_>> each time doesn't keep the waker around and (2)
-        // Shared<Future<_>> doesn't allow you to poll it if it's already completed .
-        // copying a shared future is cheap though! but this is ugly.
-        let chunk_handle = mut_inner
-            .inflight_fut
-            .get_or_insert_with(|| mut_inner.chunk.clone());
-
-        let Poll::Ready(result) = chunk_handle.poll_unpin(cx) else {
-            return Poll::Pending;
-        };
-
-        mut_inner.inflight_fut.take();
-        match result {
-            Ok(_) => Poll::Ready(Ok(mut_inner.relative_read_cursor())),
-            Err(e) if mut_inner.num_read_errors >= Self::READ_ERROR_THRESHOLD => {
-                Poll::Ready(Err(std::io::Error::new(
-                    std::io::ErrorKind::Interrupted,
-                    format!(
-                        "failed to seek to file pos {} (pos {} in volume): {e}",
-                        mut_inner.relative_read_cursor(),
-                        mut_inner.volume_read_cursor
-                    ),
-                )))
-            }
-            Err(_) => {
-                mut_inner.num_read_errors += 1;
-                let mut pinned = std::pin::pin!(mut_inner);
-                match pinned.as_mut().start_seek(SeekFrom::Current(0)) {
-                    Ok(()) => Poll::Pending,
-                    Err(e) => Poll::Ready(Err(e)),
-                }
-            }
+        match mut_inner.poll_chunk(cx) {
+            Poll::Ready(Ok(_)) => Poll::Ready(Ok(mut_inner.relative_read_cursor())),
+            Poll::Ready(Err(e)) => Poll::Ready(Err(e)),
+            Poll::Pending => Poll::Pending,
         }
     }
 }
@@ -322,60 +309,121 @@ mod test {
         object_store
     }
 
-    #[tokio::test]
-    async fn test_read_full_volume() {
-        let volume = "volume".to_string();
-        let data = random_bytes(1 << 16); // 64 KiB
-        let object_store = object_store_with_data(volume.clone(), data.clone()).await;
+    macro_rules! test_read_full_volume {
+        ($chunksize:literal) => {
+            paste::paste! {
+                #[tokio::test]
+                async fn [<test_read_full_volume_chunksize_ $chunksize>]() {
+                    let volume = "volume".to_string();
+                    let data = random_bytes(1 << 16); // 64 KiB
+                    let object_store = object_store_with_data(volume.clone(), data.clone()).await;
 
-        // volume store fetches/caches 1 KiB chunks
-        let volume_chunk_store = Arc::new(ChunkedVolumeStore::new(1 << 10, object_store));
+                    let volume_chunk_store =
+                        Arc::new(ChunkedVolumeStore::new($chunksize, object_store));
 
-        let mut file = VolumeFile::new(
-            volume_chunk_store.clone(),
-            volume,
-            ByteRange {
-                offset: 0,
-                len: data.len() as u64,
-            },
-            None,
-        )
-        .await;
-        let mut file_data = Vec::with_capacity(data.len());
-        let bytes_read = file
-            .read_to_end(&mut file_data)
-            .await
-            .expect("read should be ok");
-        assert_eq!(bytes_read, data.len());
-        assert_eq!(data.as_ref(), file_data,);
-    }
+                    let mut file = VolumeFile::new(
+                        volume_chunk_store.clone(),
+                        volume,
+                        ByteRange {
+                            offset: 0,
+                            len: data.len() as u64,
+                        },
+                        None,
+                    )
+                    .await;
 
-    #[tokio::test]
-    async fn test_read_partial_volume() {
-        let volume = "volume".to_string();
-        let data = random_bytes(1 << 16); // 64 KiB
-        let object_store = object_store_with_data(volume.clone(), data.clone()).await;
-
-        // volume store fetches/caches 1 KiB chunks
-        let volume_chunk_store = Arc::new(ChunkedVolumeStore::new(1 << 10, object_store));
-
-        // read the middle 1/3 of the volume, this is a weird number so partial chunks should be
-        // read. this maps to reading bytes (21845..43690] which doesn't get chunked up cleanly!
-        let read_range = ByteRange {
-            offset: (data.len() / 3) as u64,
-            len: (data.len() / 3) as u64,
+                    let mut file_data = Vec::with_capacity(data.len());
+                    let bytes_read = file
+                        .read_to_end(&mut file_data)
+                        .await
+                        .expect("read should be ok");
+                    assert_eq!(bytes_read, data.len());
+                    assert_eq!(data.as_ref(), file_data);
+                }
+            }
         };
-        let mut file = VolumeFile::new(volume_chunk_store.clone(), volume, read_range, None).await;
-        let mut file_data = Vec::with_capacity(data.len());
-        let bytes_read = file
-            .read_to_end(&mut file_data)
-            .await
-            .expect("read should be ok");
-        assert_eq!(bytes_read, read_range.len as usize);
-
-        let slice = data.slice(read_range.as_range_usize());
-        assert_eq!(slice.as_ref(), file_data);
     }
+    test_read_full_volume!(1024);
+    test_read_full_volume!(7);
+    test_read_full_volume!(1234);
+
+    macro_rules! test_read_partial_volume {
+        ($chunksize:literal) => {
+            paste::paste! {
+                #[tokio::test]
+                async fn [<test_read_partial_volume_chunksize_ $chunksize>]() {
+                    let volume = "volume".to_string();
+                    let data = random_bytes(1 << 16); // 64 KiB
+                    let object_store = object_store_with_data(volume.clone(), data.clone()).await;
+
+                    let volume_chunk_store = Arc::new(ChunkedVolumeStore::new($chunksize, object_store));
+
+                    // read the middle 1/3 of the volume, this is a weird number so partial chunks should be
+                    // read. this maps to reading bytes (21845..43690] which doesn't get chunked up cleanly!
+                    let read_range = ByteRange {
+                        offset: (data.len() / 3) as u64,
+                        len: (data.len() / 3) as u64,
+                    };
+                    let mut file = VolumeFile::new(volume_chunk_store.clone(), volume, read_range, None).await;
+                    let mut file_data = Vec::with_capacity(data.len());
+                    let bytes_read = file
+                        .read_to_end(&mut file_data)
+                        .await
+                        .expect("read should be ok");
+                    assert_eq!(bytes_read, read_range.len as usize);
+
+                    let slice = data.slice(read_range.as_range_usize());
+                    assert_eq!(slice.as_ref(), file_data);
+                }
+            }
+        };
+    }
+    test_read_partial_volume!(1024);
+    test_read_partial_volume!(7);
+    test_read_partial_volume!(1234);
+
+    macro_rules! test_read_full_volume_with_buf {
+        ($chunksize:literal, $bufsize:literal) => {
+            paste::paste! {
+                #[tokio::test]
+                async fn [<test_read_full_volume_with_buf_chunksize_ $chunksize _bufsize_ $bufsize>]() {
+                    let volume = "volume".to_string();
+                    let data = random_bytes(1 << 16); // 64 KiB
+                    let object_store = object_store_with_data(volume.clone(), data.clone()).await;
+
+                    // volume store fetches/caches 8 KiB chunks
+                    let volume_chunk_store = Arc::new(ChunkedVolumeStore::new($chunksize, object_store));
+
+                    let mut file = VolumeFile::new(
+                        volume_chunk_store.clone(),
+                        volume,
+                        ByteRange {
+                            offset: 0,
+                            len: data.len() as u64,
+                        },
+                        None,
+                    )
+                    .await;
+
+                    let mut file_data: Vec<u8> = Vec::with_capacity(data.len());
+                    loop {
+                        let mut buf = vec![0u8; $bufsize];
+                        let n = file.read(&mut buf).await.unwrap();
+                        file_data.extend(buf[..n].iter());
+                        if n != $bufsize {
+                            break;
+                        }
+                    }
+                    assert_eq!(data.as_ref(), file_data);
+                }
+            }
+        };
+    }
+    test_read_full_volume_with_buf!(1024, 23);
+    test_read_full_volume_with_buf!(1024, 123);
+    test_read_full_volume_with_buf!(500, 1333);
+    test_read_full_volume_with_buf!(777, 777);
+    test_read_full_volume_with_buf!(1001, 77);
 
     #[tokio::test]
     async fn test_seek_partial_volume() {
