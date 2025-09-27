db77c3775c29efa53056b69e4cd7bf6b05f9efdd
Ben Linsay <blinsay@gmail.com>
2025-09-27 12:16:33 -0400

    move pack and dump into fuse binary

---
+++ cfs-fuse/src/fuse.rs
@@ -0,0 +1,193 @@
+use cfs_core::{AsyncFileReader, Client};
+use cfs_core::{Ino, volume::Volume};
+use std::{collections::HashMap, io::SeekFrom, pin::Pin, time::Duration};
+use tokio::io::{AsyncReadExt, AsyncSeekExt};
+
+pub(crate) struct Cfs {
+    volume: Client,
+    runtime: tokio::runtime::Runtime,
+    next_fh: u64,
+    fhs: HashMap<u64, Pin<Box<dyn AsyncFileReader>>>,
+}
+
+impl Cfs {
+    pub(crate) fn new(volume: Volume, chunk_size: u64, readahead_size: u64) -> Self {
+        let runtime = tokio::runtime::Builder::new_current_thread()
+            .enable_io()
+            .enable_time()
+            .build()
+            .unwrap();
+
+        let volume = Client::new(volume, chunk_size, readahead_size);
+
+        Self {
+            volume,
+            runtime,
+            next_fh: 1,
+            fhs: Default::default(),
+        }
+    }
+
+    fn create_fh(&mut self, reader: Pin<Box<dyn AsyncFileReader>>) -> u64 {
+        // FIXME: we should check for available filehandles instead of
+        // trying to just limit this with a counter or wrapper or whatever.
+        let fh = self.next_fh;
+        self.next_fh = self.next_fh.checked_add(1).expect("filehandle overflow");
+        self.fhs.insert(fh, reader);
+        fh
+    }
+}
+
+impl fuser::Filesystem for Cfs {
+    // TODO: set up uid/gid with init
+    // TODO: set capabilities with init
+
+    fn lookup(
+        &mut self,
+        _req: &fuser::Request<'_>,
+        parent: u64,
+        name: &std::ffi::OsStr,
+        reply: fuser::ReplyEntry,
+    ) {
+        let Some(name) = name.to_str() else {
+            reply.error(libc::EINVAL);
+            return;
+        };
+        let attr = match self.volume.lookup(parent.into(), name) {
+            Ok(Some(attr)) => attr,
+            // nope
+            Ok(None) => {
+                reply.error(libc::ENOENT);
+                return;
+            }
+            // TODO: this should return EISDIR/ENOTDIR when appropriate
+            Err(_) => {
+                reply.error(libc::EIO);
+                return;
+            }
+        };
+        reply.entry(&Duration::new(0, 0), &fuse_attr(attr), 0);
+    }
+
+    fn getattr(
+        &mut self,
+        _req: &fuser::Request<'_>,
+        ino: u64,
+        _fh: Option<u64>,
+        reply: fuser::ReplyAttr,
+    ) {
+        let Ok(attr) = self.volume.getattr(ino.into()) else {
+            reply.error(libc::ENOENT);
+            return;
+        };
+        reply.attr(&Duration::new(0, 0), &fuse_attr(attr));
+    }
+
+    fn readdir(
+        &mut self,
+        _req: &fuser::Request<'_>,
+        ino: u64,
+        _fh: u64,
+        offset: i64,
+        mut reply: fuser::ReplyDirectory,
+    ) {
+        let Ok(iter) = self.volume.readdir(ino.into()) else {
+            reply.error(libc::ENOENT);
+            return;
+        };
+        let Ok(offset) = offset.try_into() else {
+            reply.error(libc::EINVAL);
+            return;
+        };
+
+        for (i, (name, attr)) in iter.enumerate().skip(offset) {
+            let is_full = reply.add(attr.ino.into(), (i + 1) as i64, fuse_kind(attr.kind), name);
+            if is_full {
+                break;
+            }
+        }
+        reply.ok();
+    }
+
+    fn open(&mut self, _req: &fuser::Request<'_>, ino: u64, _flags: i32, reply: fuser::ReplyOpen) {
+        let Ok(reader) = self.runtime.block_on(self.volume.open(ino.into())) else {
+            reply.error(libc::EIO);
+            return;
+        };
+        let fh = self.create_fh(reader);
+
+        let mut flags = 0;
+        // all special filehandles should set FOPEN_DIRECT_IO to bypass page
+        // cache both to keep values updating and to force the kernel to read
+        // a file even if we report it as length zero.
+        //
+        // https://www.kernel.org/doc/html/next/filesystems/fuse/fuse-io.html
+        if !Ino::from(ino).is_regular() {
+            flags |= fuser::consts::FOPEN_DIRECT_IO;
+        }
+
+        reply.opened(fh, flags);
+    }
+
+    fn read(
+        &mut self,
+        _req: &fuser::Request<'_>,
+        _ino: u64,
+        fh: u64,
+        offset: i64,
+        size: u32,
+        _flags: i32,
+        _lock_owner: Option<u64>,
+        reply: fuser::ReplyData,
+    ) {
+        let Some(reader) = self.fhs.get_mut(&fh) else {
+            reply.error(libc::EBADF);
+            return;
+        };
+
+        // TODO: FUSE_READ specifies offset as uint64_t in current versions,
+        // which makes it weird that it's an i64 here. cast it away for now.
+        let offset = offset as u64;
+        if offset > 0 {
+            if let Err(_e) = self.runtime.block_on(reader.seek(SeekFrom::Start(offset))) {
+                reply.error(libc::EIO);
+                return;
+            }
+        }
+
+        // TODO: re-use a scratch buffer instead of allocating here
+        let mut buf = vec![0u8; size as usize];
+        match self.runtime.block_on(reader.read(&mut buf)) {
+            Ok(n) => reply.data(&buf[..n]),
+            Err(_) => reply.error(libc::EIO),
+        }
+    }
+}
+
+fn fuse_attr(attr: &cfs_core::FileAttr) -> fuser::FileAttr {
+    fuser::FileAttr {
+        ino: attr.ino.into(),
+        size: attr.size,
+        blocks: 0, // FIXME
+        atime: attr.mtime,
+        mtime: attr.mtime,
+        ctime: attr.ctime,
+        crtime: attr.ctime,
+        kind: fuse_kind(attr.kind),
+        perm: 0o644,
+        nlink: 2,   // FIXME
+        uid: 0,     // FIXME
+        gid: 0,     // FIXME
+        rdev: 0,    // ignored
+        blksize: 0, // ignored
+        flags: 0,   // ignored
+    }
+}
+
+fn fuse_kind(kind: cfs_core::FileType) -> fuser::FileType {
+    match kind {
+        cfs_core::FileType::Regular => fuser::FileType::RegularFile,
+        cfs_core::FileType::Directory => fuser::FileType::Directory,
+        cfs_core::FileType::Symlink => fuser::FileType::Symlink,
+    }
+}
--- cfs-fuse/src/main.rs
+++ cfs-fuse/src/main.rs
@@ -1,14 +1,40 @@
+mod fuse;
 mod trace;
 
 use bytesize::ByteSize;
-use cfs_core::{AsyncFileReader, Client, Error};
+use cfs_core::Location;
 use cfs_core::{Ino, volume::Volume};
-use clap::{Parser, value_parser};
-use std::{collections::HashMap, io::SeekFrom, path::PathBuf, pin::Pin, time::Duration};
-use tokio::io::{AsyncReadExt, AsyncSeekExt};
+use clap::{Parser, Subcommand, value_parser};
+use std::io::Write;
+use std::path::Path;
+use std::path::PathBuf;
+
+use crate::fuse::Cfs;
 
 #[derive(Parser)]
-pub(crate) struct Args {
+struct Args {
+    #[command(subcommand)]
+    cmd: Cmd,
+}
+
+#[derive(Subcommand)]
+enum Cmd {
+    /// *Experimental* - Dump the metadata for a CFS volume.
+    Dump { volume: String },
+
+    /// *Experimental* - Pack a local directory into a CFS volume.
+    Pack {
+        dir: String,
+        volume: String,
+        data: String,
+    },
+
+    /// Run a cfs FUSE mount.
+    Mount(MountArgs),
+}
+
+#[derive(Parser)]
+struct MountArgs {
     #[clap(short, long, default_value_t = false)]
     debug: bool,
 
@@ -24,10 +50,6 @@ pub(crate) struct Args {
     /// The directory to mount at. Must already exist.
     mountpoint: PathBuf,
 
-    /// The bucket in object storage the volume lives in.
-    #[clap(long, default_value = "junctionlabs")]
-    bucket: String,
-
     /// The size of the chunk we fetch from object storage in a single request. It's also the size
     /// of the buffers we store within a single cache entry.
     #[clap(long, default_value = "8MiB", value_parser = value_parser!(ByteSize))]
@@ -39,12 +61,170 @@ pub(crate) struct Args {
 }
 
 fn main() {
+    tracing_subscriber::fmt::init();
+
     let args = Args::parse();
+
+    let res = match args.cmd {
+        Cmd::Dump { volume } => dump(volume),
+        Cmd::Pack { dir, volume, data } => pack(dir, volume, data),
+        Cmd::Mount(mount_args) => mount(mount_args),
+    };
+
+    if let Err(e) = res {
+        eprintln!("{e}");
+        std::process::exit(-1);
+    }
+}
+
+fn dump(volume: impl AsRef<Path>) -> anyhow::Result<()> {
+    fn location_path(l: &Location) -> String {
+        match l {
+            Location::Staged(i) => format!("staged({i})"),
+            Location::Local { path, .. } => format!("{path}", path = path.display()),
+            Location::ObjectStorage { bucket, key, .. } => format!("s3://{bucket}/{key}"),
+        }
+    }
+
+    let bs = std::fs::read(volume.as_ref()).unwrap();
+    let volume = Volume::from_bytes(&bs).unwrap();
+
+    for (name, path, attrs) in volume.walk(Ino::Root).unwrap() {
+        if path.is_empty() && !attrs.ino.is_regular() {
+            continue;
+        }
+
+        let kind;
+        let location;
+        let offset;
+        let len;
+
+        let path = {
+            let mut full_path = path;
+            full_path.push(name);
+            full_path.join("/")
+        };
+        match attrs.kind {
+            cfs_core::FileType::Regular => {
+                kind = "f";
+                let (l, b) = volume.location(attrs.ino).unwrap();
+                location = location_path(l);
+                offset = b.offset;
+                len = b.len;
+            }
+            cfs_core::FileType::Directory => {
+                kind = "d";
+                location = "".to_string();
+                offset = 0;
+                len = 0;
+            }
+            cfs_core::FileType::Symlink => todo!(),
+        }
+        println!("{kind:4} {location:16} {offset:12} {len:8} {path:40}");
+    }
+
+    Ok(())
+}
+
+fn pack(
+    dir: impl AsRef<Path>,
+    volume: impl AsRef<Path>,
+    data: impl AsRef<Path>,
+) -> anyhow::Result<()> {
+    use cfs_core::volume::Volume;
+
+    let data = data.as_ref().to_path_buf();
+    let root: &Path = dir.as_ref();
+    let mut data_f = std::fs::OpenOptions::new()
+        .create_new(true)
+        .write(true)
+        .open(&data)?;
+    let mut volume_f = std::fs::OpenOptions::new()
+        .create_new(true)
+        .write(true)
+        .open(volume)?;
+
+    let mut volume = Volume::empty();
+    let mut cursor = 0u64;
+
+    // create a staging blob
+    let staging = Location::Staged(0);
+
+    let walker = walkdir::WalkDir::new(root).min_depth(1);
+    for entry in walker {
+        let entry = entry?;
+        let path = entry.path().strip_prefix(root).unwrap();
+
+        // for a directory, just mkdir_all on the volume
+        if entry.file_type().is_dir() {
+            let dirs: Vec<_> = path
+                .components()
+                .map(|c| c.as_os_str().to_string_lossy().to_string())
+                .collect();
+            volume.mkdir_all(Ino::Root, dirs)?;
+        }
+        // for a file:
+        //
+        // - write the content into the blob as bytes
+        // - try to open the file (right now with mkdir_all, but it should maybe
+        //   be lookup_all if we know this is a dfs?)
+        // - write the file into the volume
+        //
+        // error handling here is interesting: how do we deal with a failure
+        // writing the blob? how do we deal with a failure updating the volume?
+        // both seem like they're unrecoverable.
+        if entry.file_type().is_file() {
+            let name = entry.file_name();
+            let dir = path.ancestors().nth(1).unwrap();
+            let dir_ino = if !dir.to_string_lossy().is_empty() {
+                let dirs = dir
+                    .components()
+                    .map(|c| c.as_os_str().to_string_lossy().to_string());
+                volume.mkdir_all(Ino::Root, dirs).unwrap().ino
+            } else {
+                Ino::Root
+            };
+
+            let mut file = std::fs::File::open(entry.path()).unwrap();
+            let n = std::io::copy(&mut file, &mut data_f).unwrap();
+            // eprintln!("adding {path} to blob", path = entry.path().display());
+            volume.create(
+                dir_ino,
+                name.to_string_lossy().to_string(),
+                true,
+                staging.clone(),
+                cfs_core::ByteRange {
+                    offset: cursor,
+                    len: n,
+                },
+            )?;
+
+            cursor += n;
+        }
+    }
+
+    volume.relocate(
+        &staging,
+        Location::Local {
+            path: data,
+            len: cursor as usize,
+        },
+    )?;
+
+    volume_f.write_all(&volume.to_bytes()?)?;
+
+    Ok(())
+}
+
+fn mount(args: MountArgs) -> anyhow::Result<()> {
     let volume_bs = std::fs::read(&args.volume).unwrap();
     let volume = Volume::from_bytes(&volume_bs).unwrap();
 
-    let cfs = Cfs::new(&args, volume).expect("volume should be mountable");
-
+    let cfs = Cfs::new(
+        volume,
+        args.chunk_size.as_u64(),
+        args.readahead_size.as_u64(),
+    );
     let mut opts = vec![
         fuser::MountOption::FSName("cfs".to_string()),
         fuser::MountOption::Subtype("cool".to_string()),
@@ -63,201 +243,10 @@ fn main() {
 
     if args.debug {
         tracing_subscriber::fmt::init();
-        fuser::mount2(trace::Trace::new(cfs), args.mountpoint, &opts).unwrap();
+        fuser::mount2(trace::Trace::new(cfs), args.mountpoint, &opts)?;
     } else {
-        fuser::mount2(cfs, args.mountpoint, &opts).unwrap();
-    }
-}
-
-struct Cfs {
-    volume: Client,
-    runtime: tokio::runtime::Runtime,
-    next_fh: u64,
-    fhs: HashMap<u64, Pin<Box<dyn AsyncFileReader>>>,
-}
-
-impl Cfs {
-    fn new(args: &Args, volume: Volume) -> Result<Self, Error> {
-        let runtime = tokio::runtime::Builder::new_current_thread()
-            .enable_io()
-            .enable_time()
-            .build()
-            .unwrap();
-
-        let volume = Client::new(
-            volume,
-            args.chunk_size.as_u64(),
-            args.readahead_size.as_u64(),
-        );
-
-        Ok(Self {
-            volume,
-            runtime,
-            next_fh: 1,
-            fhs: Default::default(),
-        })
+        fuser::mount2(cfs, args.mountpoint, &opts)?;
     }
 
-    fn create_fh(&mut self, reader: Pin<Box<dyn AsyncFileReader>>) -> u64 {
-        // FIXME: we should check for available filehandles instead of
-        // trying to just limit this with a counter or wrapper or whatever.
-        let fh = self.next_fh;
-        self.next_fh = self.next_fh.checked_add(1).expect("filehandle overflow");
-        self.fhs.insert(fh, reader);
-        fh
-    }
-}
-
-impl fuser::Filesystem for Cfs {
-    // TODO: set up uid/gid with init
-    // TODO: set capabilities with init
-
-    fn lookup(
-        &mut self,
-        _req: &fuser::Request<'_>,
-        parent: u64,
-        name: &std::ffi::OsStr,
-        reply: fuser::ReplyEntry,
-    ) {
-        let Some(name) = name.to_str() else {
-            reply.error(libc::EINVAL);
-            return;
-        };
-        let attr = match self.volume.lookup(parent.into(), name) {
-            Ok(Some(attr)) => attr,
-            // nope
-            Ok(None) => {
-                reply.error(libc::ENOENT);
-                return;
-            }
-            // TODO: this should return EISDIR/ENOTDIR when appropriate
-            Err(_) => {
-                reply.error(libc::EIO);
-                return;
-            }
-        };
-        reply.entry(&Duration::new(0, 0), &fuse_attr(attr), 0);
-    }
-
-    fn getattr(
-        &mut self,
-        _req: &fuser::Request<'_>,
-        ino: u64,
-        _fh: Option<u64>,
-        reply: fuser::ReplyAttr,
-    ) {
-        let Ok(attr) = self.volume.getattr(ino.into()) else {
-            reply.error(libc::ENOENT);
-            return;
-        };
-        reply.attr(&Duration::new(0, 0), &fuse_attr(attr));
-    }
-
-    fn readdir(
-        &mut self,
-        _req: &fuser::Request<'_>,
-        ino: u64,
-        _fh: u64,
-        offset: i64,
-        mut reply: fuser::ReplyDirectory,
-    ) {
-        let Ok(iter) = self.volume.readdir(ino.into()) else {
-            reply.error(libc::ENOENT);
-            return;
-        };
-        let Ok(offset) = offset.try_into() else {
-            reply.error(libc::EINVAL);
-            return;
-        };
-
-        for (i, (name, attr)) in iter.enumerate().skip(offset) {
-            let is_full = reply.add(attr.ino.into(), (i + 1) as i64, fuse_kind(attr.kind), name);
-            if is_full {
-                break;
-            }
-        }
-        reply.ok();
-    }
-
-    fn open(&mut self, _req: &fuser::Request<'_>, ino: u64, _flags: i32, reply: fuser::ReplyOpen) {
-        let Ok(reader) = self.runtime.block_on(self.volume.open(ino.into())) else {
-            reply.error(libc::EIO);
-            return;
-        };
-        let fh = self.create_fh(reader);
-
-        let mut flags = 0;
-        // all special filehandles should set FOPEN_DIRECT_IO to bypass page
-        // cache both to keep values updating and to force the kernel to read
-        // a file even if we report it as length zero.
-        //
-        // https://www.kernel.org/doc/html/next/filesystems/fuse/fuse-io.html
-        if !Ino::from(ino).is_regular() {
-            flags |= fuser::consts::FOPEN_DIRECT_IO;
-        }
-
-        reply.opened(fh, flags);
-    }
-
-    fn read(
-        &mut self,
-        _req: &fuser::Request<'_>,
-        _ino: u64,
-        fh: u64,
-        offset: i64,
-        size: u32,
-        _flags: i32,
-        _lock_owner: Option<u64>,
-        reply: fuser::ReplyData,
-    ) {
-        let Some(reader) = self.fhs.get_mut(&fh) else {
-            reply.error(libc::EBADF);
-            return;
-        };
-
-        // TODO: FUSE_READ specifies offset as uint64_t in current versions,
-        // which makes it weird that it's an i64 here. cast it away for now.
-        let offset = offset as u64;
-        if offset > 0 {
-            if let Err(_e) = self.runtime.block_on(reader.seek(SeekFrom::Start(offset))) {
-                reply.error(libc::EIO);
-                return;
-            }
-        }
-
-        // TODO: re-use a scratch buffer instead of allocating here
-        let mut buf = vec![0u8; size as usize];
-        match self.runtime.block_on(reader.read(&mut buf)) {
-            Ok(n) => reply.data(&buf[..n]),
-            Err(_) => reply.error(libc::EIO),
-        }
-    }
-}
-
-fn fuse_attr(attr: &cfs_core::FileAttr) -> fuser::FileAttr {
-    fuser::FileAttr {
-        ino: attr.ino.into(),
-        size: attr.size,
-        blocks: 0, // FIXME
-        atime: attr.mtime,
-        mtime: attr.mtime,
-        ctime: attr.ctime,
-        crtime: attr.ctime,
-        kind: fuse_kind(attr.kind),
-        perm: 0o644,
-        nlink: 2,   // FIXME
-        uid: 0,     // FIXME
-        gid: 0,     // FIXME
-        rdev: 0,    // ignored
-        blksize: 0, // ignored
-        flags: 0,   // ignored
-    }
-}
-
-fn fuse_kind(kind: cfs_core::FileType) -> fuser::FileType {
-    match kind {
-        cfs_core::FileType::Regular => fuser::FileType::RegularFile,
-        cfs_core::FileType::Directory => fuser::FileType::Directory,
-        cfs_core::FileType::Symlink => fuser::FileType::Symlink,
-    }
+    Ok(())
 }
